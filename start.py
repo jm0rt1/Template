d8f0e966f8d0555235c24be132fda6b96b805eea
Jamie Mortensen <16mortensenj@gmail.com>
2023-07-06 20:39:02 -0400

    all files for arduino project added

--- start.py
+++ start.py
@@ -1,22 +1,25 @@
 import os
 
 filename = "structure.txt"
-base_path = "."  # change this if you want to create files in a specific directory
+# change this if you want to create files in a specific directory
+base_path = os.getcwd()
 
+prev_name = []
 with open(filename, 'r') as file:
     for line in file:
         indent_count = len(line) - len(line.lstrip())
         level = indent_count // 4  # Assuming an indentation of 4 spaces.
         name = line.strip()
+        name = name.replace('/', '')
 
-        # If the name contains a ".", assume it's a file; otherwise, assume it's a directory.
-        if '.' in name:
-            # File
-            dir_path = os.path.join(base_path, *name.split('/')[:-1])
-            os.makedirs(dir_path, exist_ok=True)
-            with open(os.path.join(base_path, *name.split('/')), 'w') as _:
-                pass
+        # Adjust the current path according to the indentation level
+
+        prev_name = prev_name[:level-1] + [name]
+
+        full_path = os.path.join(base_path, *prev_name)
+
+        if '/' in line:
+            os.makedirs(full_path, exist_ok=True)
         else:
-            # Directory
-            os.makedirs(os.path.join(
-                base_path, *name.split('/')), exist_ok=True)
+            if not os.path.exists(full_path):
+                open(full_path, 'a').close()
